1f34009a2f4fb775f6610c1f116935c738b724bf
George Chaniotakis <haniotinho@gmail.com>
2019-07-29 19:45:54 +0300

    package updates + user class

---
+++ Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicRama.Models
+{
+    public class User
+    {
+        public User()
+        {
+           
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Username { get; set; }
+
+        [DataType(Datatype.Password)]
+        public string Password { get; set; }
+
+
+
+    }
+}
