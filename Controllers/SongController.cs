f80395550a247ba95e42e673eaccb71c9593bd5f
George Chaniotakis <haniotinho@gmail.com>
2019-07-31 19:53:25 +0300

    controllers

---
+++ Controllers/SongController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MusicRama.Models;
+
+
+namespace MusicRama.Controllers
+{
+    public class SongController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Song
+        public ActionResult Index()
+        {
+            return View(db.Songs);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Artist,Name,Description,Date")] Song song)
+        {
+            song.Date = DateTime.Now;
+            db.Songs.Add(song);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var song = db.Songs.Where(s => s.Id == id).ToList();
+
+            if (song == null)
+            {
+                return HttpNotFound();
+            }
+            return View(song);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Artist,Name,Description,Date")] Song song)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Database.ExecuteSqlCommand($"UPDATE Songs SET Artist = {song.Artist}, Name = {song.Name}, Description = {song.Description} WHERE Id =  {song.Id}");
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View("Song");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var songs = db.Songs.Where(s => s.Id == id).ToList();
+            foreach (Song song in songs)
+            {
+                db.Songs.Remove(song);
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MusicRama.Models;
+
+
+namespace MusicRama.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(db.Users);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include ="Id,Username,Password")] User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+           var user = db.Users.Where(u => u.Id == id).ToList();
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Username,Password")] User user)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Database.ExecuteSqlCommand($"UPDATE Users SET Username = {user.Username} WHERE Id =  {user.Id}");
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View("User");
+        }
+
+        public ActionResult Delete (int id)
+        {
+            var users = db.Users.Where(u => u.Id == id).ToList();
+            foreach (User user in users)
+            {
+                db.Users.Remove(user);
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
