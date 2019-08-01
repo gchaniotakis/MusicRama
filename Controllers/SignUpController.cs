eb3fda023103a82ae90b3df7c9549aab42d0e34a
George Chaniotakis <haniotinho@gmail.com>
2019-08-01 23:27:33 +0300

    sign up controller

---
+++ Controllers/SignUpController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using MusicRama.Models;
+
+namespace MusicRama.Controllers
+{
+    public class SignUpController : Controller
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: SignUp
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(User user)
+        {
+            (bool valid, string field, string error) = db.Users.Add(user, ModelState.IsValid);
+
+            if (!valid)
+            {
+                ModelState.AddModelError(field, error);
+                    return View(user);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
