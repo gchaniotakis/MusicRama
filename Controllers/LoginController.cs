9d7bc726026f7918d394d79a1b7d3cf17e29b738
George Chaniotakis <haniotinho@gmail.com>
2019-08-01 23:17:09 +0300

    user manager/login controller

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using MusicRama.Models;
+
+namespace MusicRama.Controllers
+{
+    public class LoginController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login (string username,string password,string returnurl)
+        {
+            UserManager manager = new UserManager();
+            var loggedInUser = manager.Login(username,password);
+
+            string decodeurl = null;
+            if (!string.IsNullOrEmpty(returnurl))
+                decodeurl = Server.UrlDecode(returnurl);
+
+            if(loggedInUser != null)
+            {
+                Session["user"] = loggedInUser;
+
+                ViewBag.Name = loggedInUser.Username;
+                return Redirect(decodeurl ?? "/home/Index");
+            }
+
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            Request.GetOwinContext().Authentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/UserManager.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace MusicRama.Models
+{
+    public class UserManager
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+
+        public User Login(string username, string password)
+        {
+            var loggedInUser = db.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+            if (loggedInUser != null)
+            {
+                var claims = new List<Claim>(new[]
+                {
+                    // adding following 2 claim just for supporting default antiforgery provider
+                    new Claim(ClaimTypes.NameIdentifier, username),
+                    new Claim(
+                        "http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider",
+                        "ASP.NET Identity", "http://www.w3.org/2001/XMLSchema#string"),
+                    new Claim(ClaimTypes.Name, username),
+                    
+                });
+
+
+
+                var identity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+
+                HttpContext.Current.GetOwinContext().Authentication.SignIn(
+                    new AuthenticationProperties { IsPersistent = false }, identity);
+            }
+
+            return loggedInUser;
+        }
+
+        public bool UserExists(string username)
+        {
+            return db.Users.Any(u => u.Username == username);
+        }
+
+
+
+    }
+}
